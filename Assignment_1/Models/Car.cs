249beb830803c1d4a9378ca9fd6d25659fab2fd8
Sergei <sbelialov@gmail.com>
2018-09-21 09:11:04 -0500

    Created 3 classes: House Person Car

---
+++ Assignment_1/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1.Models
+{
+    public class Car
+    {
+        public int Cost { get; set; }
+        public int Year { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+    }
+}
---
+++ Assignment_1/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1.Models
+{
+    public class Person
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string EMail { get; set; }
+    }
+}
